feb3867326fe0933f03a08f32cfb10918287b0ad
alirrah <alir.rah81@gmail.com>
2024-01-15 01:20:06 +0330

    Add project files.

---
+++ music app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace music_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
