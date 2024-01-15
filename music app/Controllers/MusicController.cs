c5476edac69ad3931243796cfefe145c1e481335
alirrah <alir.rah81@gmail.com>
2024-01-15 03:54:38 +0330

    custome ui panel

---
+++ music app/Controllers/MusicController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using music_app.Models;
+using System.Diagnostics;
+
+namespace music_app.Controllers
+{
+    public class MusicController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public MusicController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ music app/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using music_app.Models;
+using System.Diagnostics;
+
+namespace music_app.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public UserController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Follower()
+        {
+            return View();
+        }
+
+        public IActionResult Following()
+        {
+            return View();
+        }
+
+        public IActionResult Saved()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
